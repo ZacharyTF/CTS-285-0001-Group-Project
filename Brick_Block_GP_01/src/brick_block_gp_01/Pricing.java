9e89b349783457556b99fb0d3aaed49fdd3ee810
Travis Bivins <bivinst5220@ad.faytechcc.edu>
2021-11-22 11:25:23 -0500

    touced up on pricing and measurments
    added so user can input how many pallets, cost per pallet and the amount of bricks on it for a total cost for pallets

--- Brick_Block_GP_01/src/brick_block_gp_01/Pricing.java
+++ Brick_Block_GP_01/src/brick_block_gp_01/Pricing.java
@@ -12,6 +12,8 @@ import java.util.Scanner;
  * @author davisg0104
  */
 public class Pricing {
+    double perBrick1;
+    double perBrick2;
     public static void costPerBrick()
     {
         Scanner key = new Scanner(System.in); 
@@ -34,12 +36,44 @@ public class Pricing {
     }
     public static void costPerBricks()
     {
-        
+        Scanner key = new Scanner(System.in);
+        double perBrick1 = 0.35;
+        double perBrick2 = 0.90;
+        double amountOfBricks;
+        double cost;
+        double userBrick;
+        System.out.println("Enter the price of the brick: ");
+        userBrick = key.nextDouble();
+        if((userBrick >= perBrick1) && (userBrick <= perBrick2))
+        {
+            System.out.print("Enter the amount of bricks you getting: ");
+            amountOfBricks = key.nextDouble();
+            cost = amountOfBricks * userBrick;
+            System.out.println("We have you using " + amountOfBricks + " for a total of $" + cost);
+        }
+        else
+        {
+            System.out.println("Enter a valid price per brick ($0.35 to $0.90)");
+            costPerBricks();
+        }
     }
     
     public static void costPerPalletOfBricks()
     {
-        
+        Scanner key = new Scanner(System.in);
+        double amountOfBricks;
+        double pallet;
+        double cost;
+        double userCost, userBrick, userPallet;
+        System.out.println("How many pallets are you purchasing? ");
+        userPallet = key.nextDouble();
+        System.out.println("What is the cost per pallet? ");
+        userCost = key.nextDouble();
+        System.out.println("and how many bricks are there per pallet? ");
+        userBrick = key.nextDouble();
+        amountOfBricks = userPallet * userBrick;
+        cost = userPallet * userCost;
+        System.out.println("Right now we have you marked down for " + userPallet + " pallets for a total of " + amountOfBricks + " bricks for $" + cost);
     }
     
 }
