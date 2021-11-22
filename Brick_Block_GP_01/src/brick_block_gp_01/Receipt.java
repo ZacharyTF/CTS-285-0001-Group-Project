ad1c18b35e6f5566bb644f362b1c84fb90d8b944
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2021-11-22 11:01:03 -0500

    Added Receipt
    The receipt does not fully work as of right now. Working on getting a fully developed and working receipt.

---
+++ Brick_Block_GP_01/src/brick_block_gp_01/Receipt.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brick_block_gp_01;
+
+import java.io.*;
+import java.awt.Desktop;
+
+/**
+ *
+ * @author feganz5140
+ */
+public class Receipt {
+    public static void writeToReceipt() throws IOException
+    {
+        FileOutputStream fileStream = null;
+        PrintWriter outFS = null;
+        
+        fileStream = new FileOutputStream(name + ".txt"); //later add the date made of the receipt
+        outFS = new PrintWriter(fileStream);
+        
+        outFS.println(name + "'s Receipt"); //later add the date that is was made
+        outFS.println("======================================================");
+        outFS.println(brickType + "      " + pricePerBrick);
+        outFS.println(totalBricks + "       " + totalPriceOfBricks);
+        outFS.println(BusinessName + "        " + totalCostOfServices);
+        
+        
+        
+        //outFS.println(name);
+        
+        outFS.close();
+        
+        displayReceipt(name);
+    }
+    
+    public static void displayReceipt(String name)
+    {
+        try{
+            File file = new File(name + ".txt");
+            if(!Desktop.isDesktopSupported())
+            {
+                System.out.println("not supported");
+            }
+            Desktop desktop = Desktop.getDesktop();  
+            if(file.exists()) 
+            {
+                desktop.open(file);
+            }
+        }
+        catch(IOException e)
+        {
+        }
+    }
+}
